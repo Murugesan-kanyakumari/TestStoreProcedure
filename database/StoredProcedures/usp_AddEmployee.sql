1afa7aad035f6e4f6132a4c5491e36c9b3c412d2
nadesh-hatchster <nadesh@podiotracker.co.uk>
2026-08-08 21:17:11 +0530

    Remove redundant print statements from usp_AddEmployee procedure

--- database/StoredProcedures/usp_AddEmployee.sql
+++ database/StoredProcedures/usp_AddEmployee.sql
@@ -9,14 +9,6 @@ BEGIN
     INSERT INTO dbo.Employees (FirstName, LastName, Department)
     VALUES (@FirstName + '!!!', @LastName + 'Hello', @Department);
     print 'Employee added successfully.';
-    print 'Only this SP should redeploy now';
-    print 'heloooo';
-    print 'FINAL TEST - only this file changed>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
-    print 'FINAL TEST - only this file changed>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
-    print 'FINAL TEST - only this file changed>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
-    print 'FINAL TEST - only this file changed>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
-    print 'FINAL TEST - only this file changed>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
-    print 'FINAL TEST - only this file changed>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
 
     SELECT SCOPE_IDENTITY() AS NewEmployeeID;
 END

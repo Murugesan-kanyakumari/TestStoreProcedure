f7c45093b042d2b988c760b8d105685a3947878c
nadesh-hatchster <nadesh@podiotracker.co.uk>
2026-08-08 14:23:06 +0530

    Update fetch-depth in deploy workflow and add print statement in usp_AddEmployee procedure

--- database/StoredProcedures/usp_AddEmployee.sql
+++ database/StoredProcedures/usp_AddEmployee.sql
@@ -11,6 +11,7 @@ BEGIN
     print 'Employee added successfully.';
     print 'Only this SP should redeploy now';
     print 'heloooo';
+    print 'Haiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii';
 
     SELECT SCOPE_IDENTITY() AS NewEmployeeID;
 END
